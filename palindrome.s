e283eb323d7c62bf1878d7b1b69b76937aae614f
cthacker-udel <cthacker@udel.edu>
2021-11-17 15:24:16 -0500

    Added initialization of file handle

--- palindrome.s
+++ palindrome.s
@@ -2,4 +2,17 @@
 
 
 	main:
-			
+		mov r1, #0		@ load input type into r1, 0 as input
+		ldr r0, =InFileName	@ load file name into r0
+		swi 0x66		@ returns r0 <-- the file handle
+		ldr r1, =InFileHandle	@ load pointer to in file handle into r1
+		str r0, [ r1 ]		@stored file handle returned in InFileHandle pointer
+
+
+
+
+
+
+.data
+InFileName: .ascii "word.txt"
+InFileHandle: .word 0
