0242aaa82779cbe4c06b0c3c1430b391798d13aa
cthacker-udel <cthacker@udel.edu>
2021-11-17 15:43:16 -0500

    Added charbuffer .data label

--- palindrome.s
+++ palindrome.s
@@ -6,7 +6,11 @@
 		ldr r0, =InFileName	@ load file name into r0
 		swi 0x66		@ returns r0 <-- the file handle
 		ldr r1, =InFileHandle	@ load pointer to in file handle into r1
-		str r0, [ r1 ]		@stored file handle returned in InFileHandle pointer
+		str r0, [ r1 ]		@ stored file handle returned in InFileHandle pointer
+		mov r0, #1		@ before malloc
+		swi 0x12		@ return r0 <-- addr of block
+		ldr r1, =CharBuffer	@ loading CharBuffer pointer into r1
+		str r0, [r1]		@ store 1 byte block in CharBuffer
 
 
 
@@ -16,3 +20,4 @@
 .data
 InFileName: .ascii "word.txt"
 InFileHandle: .word 0
+CharBuffer: .word 0
